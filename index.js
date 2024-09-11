e3540e7bc82bd49ddc52e1c8b72b7346564c228c
Muhammad Saad Masood <mrsaadmasood1@gmail.com>
2024-09-11 15:59:31 +0000

    add shortned

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require("dns")
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended : true}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,10 +17,33 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+let counter = 0;
+let map = {}
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.post("/api/shorturl", (req, res)=>{
+  const { url } = req.body
+  if(!url.startsWith("https://"))
+    return res.json({ error : "invalid url"})
+  let urlToUse = new URL(url)
+  dns.lookup(urlToUse.hostname.replace("www.", ""), 
+  (err)=>{
+    if(err) return res.json({ error : "invalid url"})
+    counter++
+    map[counter.toString()] = url;
+    return res.json({
+      original_url: url,
+      shorturl : counter
+    })
+  })
+})
+
+app.get("/api/shorturl/:shorturl", (req, res)=>{
+  const shorturl = req.params.shorturl;
+  const redirectionUrl = map[shorturl]
+  if(!redirectionUrl) return res.json({ error : "invalid url"})
+  return res.redirect(redirectionUrl)
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
