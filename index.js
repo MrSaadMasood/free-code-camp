2b7c3ba2528fafc001abb402d97b04d81f4b838a
Muhammad Saad Masood <mrsaadmasood1@gmail.com>
2024-09-11 18:49:23 +0000

    added

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require("multer")
+const upload = multer({ desc : "uploads/"})
 
 var app = express();
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended : false}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res)=>{
+  const { originalname, mimetype, size } =req.file
+  return res.json({
+    name : originalname,
+    type : mimetype,
+    size
+  })
+})
 
 
 
